adb2e6a1484c7d9123236e018dbc4b5f3e975f59
Andrei Kisliuk <kisliuk.andrei@gmailcom>
2021-02-14 18:36:43 +0100

    feat: add the script to the index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
+  if (array !== undefined && array.length !== 0) {
+    return Math.min(...array)
+  }    
+    
   return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array !== undefined && array.length !== 0) {
+        return Math.max(...array)
+    }
+     
+    return 0; 
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array !== undefined && array.length !== 0) {
+        let num = array.reduce((prev, cur) => prev + cur);
+        let avgNum = num / array.length;
+        return avgNum
+    } 
+    
+    return 0;
 }
